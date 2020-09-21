ec9fd0dd3649e810e9b27143bfe3e28aea1450f8
unknown <csmezba@gmail.com>
2020-09-22 02:30:10 +0600

    router segrigation: separation of concern

--- router/baseRouter.js
+++ router/baseRouter.js
@@ -1,27 +1,5 @@
-const router = require('express').Router(),
-axios = require('axios'),
-apiConfig = require('../Config.js').API_CONFIG
-countries = require('../Constents.js').COUNTRIES;
+const router = require('express').Router()
 
-router.get('/weather', (req, res) => {
-  fetchWeatherAsync().then(response => res.json(response.data))
-  .catch(err => res.json(err.message || 'internal server error!!'));
-})
-
-async function fetchWeatherAsync(){
-  const url = apiConfig.getFullUrl(countries.BD.DHAKA);
-  console.log(url);
-  console.log("http://api.openweathermap.org/data/2.5/weather?q=Dhaka,BD&appid=a5f490d51fc479a6f031e33819698f78&units=metric");
-  let response = await axios.get(url);
-  return response;
-}
-
-function fetchWeatherPromise(){
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=Dhaka,BD&appid=a5f490d51fc479a6f031e33819698f78&units=metric";
-  return new Promise((resolve, reject) => {
-    axios.get(url).then(response => resolve(response))
-    .catch(err => reject(err));
-  });
-}
+router.use('/weather', require('./weatherRouter'))
 
 module.exports = router;
